39150f8108af04eebc310320f52d2bbdf3a8c647
Doug Hilpipre <dhilpipre@newrelic.com>
2025-10-09 08:01:57 -0500

    simplified code and returned to async tokens

--- reactor-core-2.x/src/main/java/reactor/core/dispatch/AbstractLifecycleDispatcher.java
+++
@@ -1,9 +0,0 @@
-package reactor.core.dispatch;
-
-import com.newrelic.api.agent.weaver.MatchType;
-import com.newrelic.api.agent.weaver.Weave;
-
-@Weave(type=MatchType.BaseClass)
-public abstract class AbstractLifecycleDispatcher {
-
-}
